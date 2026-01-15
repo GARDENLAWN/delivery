5ff106b6c5ae34cb17fe63a31b341200ac1202da
GARDENLAWN <rpcsky@gmail.com>
2026-01-15 17:47:49 +0100

    Init

--- Model/Config/Source/LoadType.php
+++ Model/Config/Source/LoadType.php
@@ -8,14 +8,54 @@ class LoadType implements OptionSourceInterface
     public function toOptionArray()
     {
         return [
-            ['value' => '2_europalette', 'label' => __('Europallet (120x80)')],
-            ['value' => '1_other_palette', 'label' => __('Other Pallet')],
-            ['value' => '10_box', 'label' => __('Box (Karton)')],
-            ['value' => '11_crates', 'label' => __('Crates (Skrzyniopaleta)')],
-            ['value' => '19_big_bag', 'label' => __('Big Bag')],
-            ['value' => '20_long_load', 'label' => __('Long Load (Dłużyca)')],
-            ['value' => '23_machinery', 'label' => __('Machinery (Maszyny)')],
-            ['value' => '24_oversize', 'label' => __('Oversize (Gabaryt)')],
+            // Palety
+            ['value' => '2_europalette', 'label' => __('Europallet (120 x 80 cm)')],
+            ['value' => '1_palette', 'label' => __('Custom Pallet (Paleta niestandardowa)')],
+            ['value' => '34_eur_6', 'label' => __('EUR 6 Pallet (80 x 60 cm)')],
+            ['value' => '35_eur_2', 'label' => __('EUR 2 Pallet (120 x 100 cm)')],
+            ['value' => '36_eur_3', 'label' => __('EUR 3 Pallet (100 x 120 cm)')],
+            ['value' => '37_container_palette', 'label' => __('Container Pallet (114 x 114 cm)')],
+            ['value' => '38_oversized', 'label' => __('Oversized Pallet (120 x 120 cm)')],
+
+            // Inne typy
+            ['value' => '3_big_bag', 'label' => __('Big Bag (90 x 90 cm)')],
+            ['value' => '4_log', 'label' => __('Log (Dłużyca)')],
+            ['value' => '5_bag', 'label' => __('Bag (Worek 90 x 90 cm)')],
+            ['value' => '6_barrel', 'label' => __('Barrel (Beczka)')],
+            ['value' => '7_carton', 'label' => __('Carton (Karton)')],
+            ['value' => '8_piece', 'label' => __('Piece (Sztuka)')],
+            ['value' => '9_cubic', 'label' => __('Bulk Materials (Materiały sypkie)')],
+            ['value' => '10_box', 'label' => __('Box (Skrzynia)')],
+            ['value' => '11_other', 'label' => __('Other (Inny)')],
+            ['value' => '12_roll', 'label' => __('Roll (Rolka)')],
+            ['value' => '11_crates', 'label' => __('Crates (Skrzyniopaleta)')], // Zachowane ze starej listy, jeśli używane
+            ['value' => '23_machinery', 'label' => __('Machinery (Maszyny)')], // Zachowane
+            ['value' => '24_oversize', 'label' => __('Oversize (Gabaryt)')], // Zachowane
+
+            // Palety chemiczne
+            ['value' => '13_cp1_chemical_palette', 'label' => __('CP1 Chemical Pallet (100 x 120 cm)')],
+            ['value' => '14_cp2_chemical_palette', 'label' => __('CP2 Chemical Pallet (80 x 120 cm)')],
+            ['value' => '15_cp3_chemical_palette', 'label' => __('CP3 Chemical Pallet (114 x 114 cm)')],
+            ['value' => '16_cp4_chemical_palette', 'label' => __('CP4 Chemical Pallet (111 x 130 cm)')],
+            ['value' => '17_cp5_chemical_palette', 'label' => __('CP5 Chemical Pallet (76 x 114 cm)')],
+            ['value' => '18_cp6_chemical_palette', 'label' => __('CP6 Chemical Pallet (100 x 120 cm)')],
+            ['value' => '19_cp7_chemical_palette', 'label' => __('CP7 Chemical Pallet (111 x 130 cm)')],
+            ['value' => '20_cp8_chemical_palette', 'label' => __('CP8 Chemical Pallet (114 x 114 cm)')],
+            ['value' => '21_cp9_chemical_palette', 'label' => __('CP9 Chemical Pallet (114 x 114 cm)')],
+
+            // Kontenery
+            ['value' => '22_20gp_dry_van', 'label' => __('20GP Dry Van')],
+            ['value' => '23_40gp_dry_van', 'label' => __('40GP Dry Van')],
+            ['value' => '24_40hc_high_cube', 'label' => __('40HC High Cube')],
+            ['value' => '25_45hc_high_cube', 'label' => __('45HC High Cube')],
+            ['value' => '26_20re_temperature_controlled', 'label' => __('20RE Reefer')],
+            ['value' => '27_40re_temperature_controlled', 'label' => __('40RE Reefer')],
+            ['value' => '28_40rh_temperature_controlled_high_cube', 'label' => __('40RH Reefer High Cube')],
+            ['value' => '29_20ot_open_top', 'label' => __('20OT Open Top')],
+            ['value' => '30_40ot_open_top', 'label' => __('40OT Open Top')],
+            ['value' => '31_40hw_palette_high_cube', 'label' => __('40HW Pallet High Cube')],
+            ['value' => '32_45hw_palette_high_cube', 'label' => __('45HW Pallet High Cube')],
+            ['value' => '33_20vh_ventilated_container', 'label' => __('20VH Ventilated')],
         ];
     }
 }
--- Model/Config/Source/VehicleBody.php
+++ Model/Config/Source/VehicleBody.php
@@ -8,14 +8,30 @@ class VehicleBody implements OptionSourceInterface
     public function toOptionArray()
     {
         return [
+            // Plandeka
             ['value' => '9_curtainsider', 'label' => __('Curtainsider (Firanka)')],
             ['value' => '8_standard_tent', 'label' => __('Standard Tent (Plandeka)')],
-            ['value' => '1_box', 'label' => __('Box (Kontener)')],
+
+            // Sztywna zabudowa
+            ['value' => '42_open_box', 'label' => __('Open Box (Skrzynia)')],
+            ['value' => '19_box', 'label' => __('Box (Sztywna zabudowa)')],
+
+            // Chłodnia
+            ['value' => '1_cooler', 'label' => __('Cooler (Chłodnia)')],
+            ['value' => '43_meathanging', 'label' => __('Meat Hanging (Hakówka)')],
             ['value' => '2_isotherm', 'label' => __('Isotherm (Izoterma)')],
-            ['value' => '3_cooler', 'label' => __('Cooler (Chłodnia)')],
+
+            // Dowolna / Inne
+            ['value' => '47_other_tanker', 'label' => __('Other Tanker (Inna cysterna)')],
+            ['value' => '15_other', 'label' => __('Other (Inne)')],
+            ['value' => '14_tow_truck', 'label' => __('Tow Truck (Laweta)')],
+            ['value' => '20_dump_truck', 'label' => __('Dump Truck (Wywrotka)')],
+            ['value' => '49_steel_tipper_truck', 'label' => __('Steel Tipper (Wywrotka stalowa)')],
+            ['value' => '50_aluminum_tipper_truck', 'label' => __('Aluminum Tipper (Wywrotka aluminiowa)')],
+
+            // Pozostałe z poprzedniej listy (dla kompatybilności, jeśli używane)
             ['value' => '12_mega', 'label' => __('Mega')],
             ['value' => '17_open', 'label' => __('Open (Odkryty)')],
-            ['value' => '18_tipper', 'label' => __('Tipper (Wywrotka)')],
             ['value' => '21_platform', 'label' => __('Platform (Platforma)')],
             ['value' => '22_low_loader', 'label' => __('Low Loader (Niskopodwoziowy)')],
         ];
